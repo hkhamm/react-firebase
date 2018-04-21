a4a6b168ed9f17abfacf04f2950af45f449a590a
Keith Hamm <hkhamm@gmail.com>
2018-04-21 13:29:01 -0700

    firebase+react works

--- src/stores/AppStore.tsx
+++ src/stores/AppStore.tsx
@@ -1,7 +1,34 @@
-import { observable } from "mobx"
+import * as firebase from "firebase"
+import "firebase/firestore"
+import { autorun, observable } from "mobx"
+
+type Firestore = firebase.firestore.Firestore
+type DocumentSnapshot = firebase.firestore.DocumentSnapshot
+type DocumentReference = firebase.firestore.DocumentReference
 
 export class AppStore {
+
     @observable public count: number = 0
+
+    constructor() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyDqVtgc9Dq1EF2HjQRv3iJnSmQTYEixN_Q",
+            authDomain: "hello-react-f7f06.firebaseapp.com",
+            projectId: "hello-react-f7f06"
+        })
+
+        const firestore: Firestore = firebase.firestore()
+        const database: DocumentReference = firestore.collection("default").doc("default")
+        database.get().then((snapshot: DocumentSnapshot) => {
+            this.count = snapshot.get("count")
+            autorun(() => {
+                database.set({
+                    count: this.count
+                })
+            })
+        })
+    }
+
 }
 
 export const appStore = new AppStore()
