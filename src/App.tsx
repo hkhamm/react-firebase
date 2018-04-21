16c017bc8b44646107e6ac05309b11a2e315eaa0
Keith Hamm <hkhamm@gmail.com>
2018-04-21 10:18:41 -0700

    adds decrement button and moves onclick actions into methods

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,39 @@ interface Props {
 @observer
 export default class App extends React.Component<Props, {}> {
 
+    constructor(props: Props) {
+        super(props)
+        this.onIncrement = this.onIncrement.bind(this)
+        this.onDecrement = this.onDecrement.bind(this)
+    }
+
     public render() {
         return (
-            <div>
-                <label>Count {this.props.store.count}</label>
-                <button id={"increment"} onClick={() => this.props.store.count++}>Increment</button>
+            <div style={{
+                width: "50%",
+                margin: "auto",
+                paddingTop: "2rem",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <div>
+                    <label>Count {this.props.store.count}</label>
+                    <div><button onClick={this.onIncrement}>Increment</button></div>
+                    <div><button onClick={this.onDecrement}>Decrement</button></div>
+                </div>
             </div>
         )
+    }
+
+    private onIncrement() {
+        this.props.store.count++
+    }
+
+    private onDecrement() {
+        if (this.props.store.count > 0) {
+            this.props.store.count--
+        }
     }
 
 }
